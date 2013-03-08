24590bf15c9ab5a8c80ae5a14a6cc19edd212738
Chris Wyatt <clwyatt@vt.edu>
2013-03-08 13:35:57 -0500

    Optimizations of Interior Point optimization
    * implemented improved precomputation when possible
    * reduced unecessary allocation/deallocations
    * rewrote some loops using raw memory and array indexing to improve loop unrolling

--- Code/Optimizer.cxx
+++ Code/Optimizer.cxx
@@ -25,6 +25,14 @@ Optimizer::Optimizer(const std::vector<DiffusionEncodingDirection> &encodings)
 
   unsigned int N = nonzero_encodings.size();
 
+  // create a timeSchedule for interior-point method
+  double t = t0;
+  while( (3*N)/t > teps )
+    {
+      timeSchedule.push_back(t);
+      t = tmu*t;
+    }
+
   // A is N x 21
   A = vnl_matrix<double>(N, 21);
   for(unsigned int i = 0; i < N; ++i)
@@ -32,9 +40,6 @@ Optimizer::Optimizer(const std::vector<DiffusionEncodingDirection> &encodings)
       DiffusionEncodingDirection n = nonzero_encodings[i];
       make_A(i, n, A);
     }
-
-  // precompute some terms
-  ATA = vnl_transpose(A)*A;
 
   // C is 3N x 21
   C = vnl_matrix<double>(3*N, 21);
@@ -58,6 +63,75 @@ Optimizer::Optimizer(const std::vector<DiffusionEncodingDirection> &encodings)
   // B is N x 1
   B = vnl_vector<double>(N);
 
+  grad = vnl_vector<double>(21);
+  H = vnl_matrix<double>(21,21);
+  build_ATA();
+  build_CCT();
+  build_d();
+}
+
+Optimizer::~Optimizer()
+{
+  delete [] ATA;
+  delete [] ATB;
+  delete [] CCT;
+  delete [] d;
+}
+
+void Optimizer::build_ATA()
+{
+  unsigned int T = timeSchedule.size();
+
+  ATA = new double[21*21*T];
+
+  for(unsigned int t = 0; t < T; ++t)
+    {
+      vnl_matrix<double> TEMP = vnl_diag_matrix<double>(21, 2*timeSchedule[t])*(vnl_transpose(A)*A);
+      for(unsigned int r = 0; r < 21; ++r)
+	for(unsigned int c = 0; c < 21; ++c)
+	  {
+	    ATA[t*21*21 + r*21 + c] =  TEMP[r][c];
+	  }
+    }
+}
+
+void Optimizer::build_ATB()
+{
+  unsigned int T = timeSchedule.size();
+
+  ATB = new double[21*T];
+
+  for(unsigned int t = 0; t < T; ++t)
+    {
+      vnl_vector<double> TEMP = vnl_diag_matrix<double>(21, 2*timeSchedule[t])*(vnl_transpose(A)*B);
+      for(unsigned int c = 0; c < 21; ++c)
+	  {
+	    ATB[t*21 + c] =  TEMP[c];
+	  }
+    }
+}
+
+void Optimizer::build_CCT()
+{
+  unsigned int N = nonzero_encodings.size();
+
+  CCT = new double[21*21*3*N];
+
+  for(unsigned int i = 0; i < 3*N; ++i)
+    {
+      vnl_matrix<double> TEMP = outer_product(C.get_row(i), C.get_row(i));
+      for(unsigned int r = 0; r < 21; ++r)
+	for(unsigned int c = 0; c < 21; ++c)
+	  {
+	    CCT[i*21*21 + r*21 + c] =  TEMP[r][c];
+	  }
+    }
+}
+
+void Optimizer::build_d()
+{
+  unsigned int N = nonzero_encodings.size();
+  d = new double[3*N];
 }
 
 void Optimizer::SetDWI(double * data)
@@ -88,7 +162,7 @@ void Optimizer::SetDWI(double * data)
     }
 
   // precompute
-  ATB = vnl_transpose(A)*B;
+  build_ATB();
 }
 
 unsigned int Optimizer::violated_constraints(vnl_vector_fixed<double, 21> &X)
@@ -121,48 +195,57 @@ double Optimizer::ULLS(vnl_vector_fixed<double, 21> & X)
   return norm;
 }
 
-void Optimizer::Newton(double t, vnl_vector_fixed<double, 21> & X)
+void Optimizer::Newton(unsigned int timeIndex, vnl_vector_fixed<double, 21> & X)
 {
-  // std::cout << "Starting Newton with t = " << t;
-  // std::cout << " and solution norm = " << SolutionNorm(X) << std::endl;
-
   unsigned int N = nonzero_encodings.size();
 
-  vnl_vector<double> grad;
-  vnl_matrix<double> H;
-  vnl_matrix<double> Hinit = vnl_diag_matrix<double>(21,2*t)*ATA;
   double updatenorm;
   const double eps = 1e-6;
   do
     {
-      vnl_vector<double> d = C*X;
-      grad = vnl_diag_matrix<double>(21,2*t)*(ATA*X-ATB);
-      for(unsigned int i = 0; i < 3*N; ++i)
+      for(unsigned int r = 0; r < 3*N; ++r)
 	{
-	  grad += (C.get_row(i))/(-d[i]);
+	  d[r] = 0;
+	  for(unsigned int c = 0; c < 21; ++c) d[r] += C[r][c]*X[c];
 	}
 
-      H = Hinit;
+      for(unsigned int i = 0; i < 21; ++i)
+	{
+	  grad[i] = 0;
+	  for(unsigned int j = 0; j < 21; ++j)
+	    {
+	      grad[i] += ATA[21*21*timeIndex + 21*i + j]*X[j];
+	    }
+	  grad[i] -= ATB[21*timeIndex + i];
+	}
+
       for(unsigned int i = 0; i < 3*N; ++i)
 	{
-	  vnl_vector<double> temp = C.get_row(i);
+	  for(unsigned int j = 0; j < 21; ++j)
+	    grad[j] += -C[i][j]/d[i];
+	}
+
+      for(unsigned int r = 0; r < 21; ++r)
+	for(unsigned int c = 0; c < 21; ++c)
+	  {
+	    H[r][c] = ATA[21*21*timeIndex + 21*r + c];
+	  }
+
+      for(unsigned int i = 0; i < 3*N; ++i)
+	{
 	  double constraintsq = d[i]*d[i];
 	  for(unsigned int r = 0; r < 21; ++r)
 	    for(unsigned int c = 0; c < 21; ++c)
 	      {
-		H[r][c] += (temp[r]*temp[c])/(constraintsq);
+		H[r][c] += CCT[i*21*21 + r*21 + c]/constraintsq;
 	      }
 	}
 
-      vnl_vector<double> update =  vnl_svd<double>(H).solve(grad);
+      vnl_vector<double> update = vnl_svd<double>(H).solve(grad);
       X = X - update;
-
       updatenorm = norm(update);
-
-      //std::cout << "Newton update norm: " << updatenorm << std::endl;
     }
   while(updatenorm > eps);
-
 }
 
 double Optimizer::SolutionNorm(vnl_vector_fixed<double, 21> & X)
@@ -181,13 +264,9 @@ double Optimizer::SolutionNorm(vnl_vector_fixed<double, 21> & X)
 
 double Optimizer::InteriorPointMethod(vnl_vector_fixed<double, 21> & X)
 {
-  unsigned int N = nonzero_encodings.size();
-
-  double t = t0;
-  while( (3*N)/t > teps )
+  for(unsigned int timeIndex = 0; timeIndex < timeSchedule.size(); ++timeIndex)
     {
-      Newton(t, X);
-      t = tmu*t;
+      Newton(timeIndex, X);
       if(violated_constraints(X) > 0)
 	{
 	  std::cout << "WARNING: Interior Point Method Halted Early." << std::endl;
--- Code/Optimizer.h
+++ Code/Optimizer.h
@@ -9,6 +9,7 @@ class Optimizer
 {
 public:
   Optimizer(const std::vector<DiffusionEncodingDirection> &encodings);
+  ~Optimizer();
 
   void SetDWI(double * data);
 
@@ -16,10 +17,14 @@ public:
 
 private:
   Optimizer(){};
+  void build_ATA();
+  void build_ATB();
+  void build_CCT();
+  void build_d();
   unsigned int violated_constraints(vnl_vector_fixed<double, 21> &X);
   double ULLS(vnl_vector_fixed<double, 21> & X);
   double InteriorPointMethod(vnl_vector_fixed<double, 21> & X);
-  void Newton(double t, vnl_vector_fixed<double, 21> & X);
+  void Newton(unsigned int timeIndex, vnl_vector_fixed<double, 21> & X);
   double SolutionNorm(vnl_vector_fixed<double, 21> & X);
 
   std::vector<DiffusionEncodingDirection> full_encodings;
@@ -29,8 +34,16 @@ private:
   static const double t0 = 1e-8;
   static const double tmu = 1.5;
   static const double teps = 0.01;
+  std::vector<double> timeSchedule;
 
-  vnl_matrix<double> A, C, ATA;
-  vnl_vector<double> B, ATB;
+  vnl_matrix<double> A, C;
+  vnl_vector<double> B;
 
+  vnl_vector<double> grad;
+  vnl_matrix<double> H;
+
+  double * ATA;
+  double * ATB;
+  double * CCT;
+  double * d;
 };
